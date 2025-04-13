692e1daf9d763a0bfa46b89e2f9e8a7a5bd4c184
colinskorir <korircoll20@gmail.com>
2025-04-13 18:15:44 +0300

    files added

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function SearchBar({ setSearchTerm }) {
   return (
-   
+    <input
+      type="text"
+      placeholder="Search expenses..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+      className="search-bar"
+    />
   );
 }
 
