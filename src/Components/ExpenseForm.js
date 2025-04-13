15e0c218a1a77bc7254a03c195e51c12bfa304dc
colinskorir <korircoll20@gmail.com>
2025-04-13 18:13:47 +0300

    files added

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -26,3 +26,19 @@ function ExpenseForm({ addExpense }) {
       });
     }
   };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="expense-form">
+      <input
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={handleChange}
+        placeholder="Description"
+        required
+      />
+      
